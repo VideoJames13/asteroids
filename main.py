37e85272d75d514ceb0b00ff7c5d56a1dd85b52d
VideoJames13 <JamesMehran1@gmail.com>
2025-06-25 16:02:53 -0400

    added circleshape and player, which handle creation of all in-game objects, and created the player triangle in the main game loop

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     print("Starting Asteroids!")
@@ -15,7 +19,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        
+        player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player1.draw(screen)
+                         
         pygame.display.flip()
+        
+        #limit framerate to 60fps
+        dt = (clock.tick(60)/1000)
+
+        
 
 
 
